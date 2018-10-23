4150139ac9b92916d6a41540d0b15dd68c5ba11d
hamzahfauzy <hamzahfauzy97@gmail.com>
2018-10-23 18:48:35 +0700

    add jquery from cdn

--- emusrenbang/views/laporan-bappeda/bappeda.php
+++ emusrenbang/views/laporan-bappeda/bappeda.php
@@ -550,7 +550,7 @@ $this->params['breadcrumbs'][] = $this->title;
   </div>
 </div>
 
-<script src="/eperencanaan/emusrenbang/web/assets/687adee6/jquery.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 $(".urusan").change(function(){
 	var val = $(".urusan").val()

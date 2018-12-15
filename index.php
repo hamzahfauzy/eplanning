87ceaeba1772cbd7c19d6dbc9ac4541c6bad4cac
Eplanning Asahan <eplanningasahan@gmail.com>
2018-12-15 10:57:12 +0700

    index live

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* Redirect browser */
-header("Location: http://".$_SERVER['SERVER_NAME']."/eplanning/eperencanaan/web");
+header("Location: http://".$_SERVER['SERVER_NAME']."/2019/eperencanaan/web");
  
 /* Make sure that code below does not get executed when we redirect. */
 exit;
